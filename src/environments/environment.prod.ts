48d713324778400c71306f7ec2360594ad6c57a6
nickrockstar <wezawe@gmail.com>
2018-08-21 09:40:47 +0200

    Env

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    googleClientId: '126341721679-s4d52tllits312rd649gv2n4gbniak1q.apps.googleusercontent.com',
-    api_url: 'http://localhost:5000/hire-by-hellocrowd/us-central1/api'
-};
