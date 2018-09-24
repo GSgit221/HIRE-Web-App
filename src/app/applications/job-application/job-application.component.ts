c18b8d40a1c6510e03298ce6e11e2f2172317f25
lionmof <sashbondar@yandex.ru>
2018-09-24 20:18:40 +0300

    first view

--- src/app/applications/job-application/job-application.component.ts
+++ src/app/applications/job-application/job-application.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as closest from 'closest';
+
 
 @Component({
-  selector: 'app-job-application',
-  templateUrl: './job-application.component.html',
-  styleUrls: ['./job-application.component.scss']
+    selector: 'app-job-application',
+    templateUrl: './job-application.component.html',
+    styleUrls: ['./job-application.component.scss']
 })
 export class JobApplicationComponent implements OnInit {
+    mainColor = '#72BF44';
+    activeSection = 'start';
+    constructor() {
+    }
 
-  constructor() { }
+    ngOnInit() {
+    }
+    onChangeSection(section: string) {
+        this.activeSection = section;
+    }
+    onFocusInput(event) {
+        closest(event.target, '.form-block-col').classList.add('focus');
 
-  ngOnInit() {
-  }
+    }
+    onBlurInput(event) {
+        closest(event.target, '.form-block-col').classList.remove('focus');
+    }
 
 }
