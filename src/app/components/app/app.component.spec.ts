801104bebf2fe3e69363802452b218812e8f9e31
nickrockstar <wezawe@gmail.com>
2019-05-02 01:59:49 +0200

    Unnecessary spec files removed

--- src/app/components/app/app.component.spec.ts
+++ src/app/components/app/app.component.spec.ts
@@ -1,27 +1,35 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { combineReducers, Store, StoreModule } from '@ngrx/store';
+import * as fromRoot from '../../store';
+import * as fromActions from '../../store/actions';
+import * as fromReducers from '../../store/reducers';
+import { AuthService } from './../../modules/auth/auth.service';
+import { SharedModule } from './../../modules/shared/shared.module';
+import { PageNotFoundComponent } from './../page-not-found/page-not-found.component';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [AppComponent]
+            declarations: [AppComponent, PageNotFoundComponent],
+            schemas: [NO_ERRORS_SCHEMA],
+            imports: [
+                SharedModule,
+                RouterTestingModule,
+                StoreModule.forRoot({
+                    ...fromRoot.reducers,
+                    products: combineReducers(fromReducers.reducers)
+                })
+            ],
+            providers: [AuthService]
         }).compileComponents();
     }));
     it('should create the app', async(() => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
-    }));
-    it(`should have as title 'app'`, async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual('app');
-    }));
-    it('should render title in a h1 tag', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain('Welcome to hc-hire!');
     }));
 });
--- src/app/pipes/account-type.pipe.spec.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable:no-unused-variable */
-import { AccountTypePipe } from './account-type.pipe';
-
-describe('Pipe: AccountTypee', () => {
-    it('create an instance', () => {
-        const pipe = new AccountTypePipe();
-        expect(pipe).toBeTruthy();
-    });
-});
