afd73ce75b8dd493bb909556697e1a1dd4a018c4
chetan-techtic <chetan@techtic.com>
2019-05-29 19:25:32 +0530

    append placeholder value in email

--- src/app/modules/settings/email-templates/email-template-item/email-template-item.component.ts
+++ src/app/modules/settings/email-templates/email-template-item/email-template-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
@@ -10,6 +10,9 @@ import * as fromStore from '../store';
 import * as fromStoreActions from '../store/actions/emails.action';
 import * as fromStoreSelectors from '../store/selectors/emails.selector';
 import { FormHelperService } from './../../../../core/services/form-helper.service';
+import { DomHandler } from './../../../../libs/editor/domhandler';
+
+declare var Quill: any;
 
 @Component({
     selector: 'app-email-template-item',
@@ -21,6 +24,8 @@ export class EmailTemplateItemComponent implements OnInit {
     contentLoading = true;
     initialLoad = true;
     emailTemplateId: string;
+    cursorPosition: any;
+    textContent: any;
     itemForm: FormGroup;
     msgs: Message[] = [];
     InsertPlaceholders: SelectItem[];
@@ -41,7 +46,8 @@ export class EmailTemplateItemComponent implements OnInit {
         private route: ActivatedRoute,
         private fb: FormBuilder,
         private formHelper: FormHelperService,
-        private store: Store<fromStore.EmailsState>
+        private store: Store<fromStore.EmailsState>,
+        public el: ElementRef
     ) {
         this.emailTemplateId = this.route.snapshot.url[0].path;
         this.itemForm = this.fb.group({
@@ -69,16 +75,110 @@ export class EmailTemplateItemComponent implements OnInit {
         });
 
         this.InsertPlaceholders = [
-            { label: 'candidate_name', value: '{{ candidate_name }}' },
-            { label: 'missing_fields', value: '{{ missing_fields }}' },
-            { label: 'sender_email', value: '{{ sender_email }}' },
-            { label: 'sender_name', value: '{{ sender_name }}' },
-            { label: 'job_title', value: '{{ job_title }}' },
-            { label: 'sender_company', value: '{{ sender_company }}' }
+            { label: 'candidate_name', value: '{{candidate_name}}' },
+            { label: 'missing_fields', value: '{{missing_fields}}' },
+            { label: 'sender_email', value: '{{sender_email}}' },
+            { label: 'sender_name', value: '{{sender_name}}' },
+            { label: 'job_title', value: '{{job_title}}' },
+            { label: 'sender_company', value: '{{sender_company}}' },
+            { label: 'tenant_name', value: '{{tenant_name}}' },
+            { label: 'recruiter', value: '{{recruiter}}' },
+            { label: 'job_owner', value: '{{job_owner}}' }
         ];
     }
+    @Input() get readonly(): boolean {
+        return this._readonly;
+    }
+
+    set readonly(val: boolean) {
+        this._readonly = val;
+
+        if (this.quill) {
+            if (this._readonly) {
+                this.quill.disable();
+            } else {
+                this.quill.enable();
+            }
+        }
+    }
+
+    @Output() onTextChange: EventEmitter<any> = new EventEmitter();
+
+    @Output() onSelectionChange: EventEmitter<any> = new EventEmitter();
+
+    @Input() placeholder: string;
+
+    @Input() formats: string[];
+
+    @Input() modules: any;
+
+    @Input() bounds: Element;
+
+    @Input() scrollingContainer: Element;
+
+    @Input() debug: string;
+
+    @Output() onInit: EventEmitter<any> = new EventEmitter();
+
+    value: string;
+
+    _readonly: boolean;
+
+    quill: any;
 
     ngOnInit() {}
+
+    ngAfterViewInit() {
+        const editorElement = DomHandler.findSingle(this.el.nativeElement, 'div.ui-editor-content');
+        const toolbarElement = DomHandler.findSingle(this.el.nativeElement, 'div.ui-editor-toolbar');
+        const defaultModule = { toolbar: toolbarElement };
+        const modules = this.modules ? { ...defaultModule, ...this.modules } : defaultModule;
+
+        this.quill = new Quill(editorElement, {
+            modules,
+            placeholder: this.placeholder,
+            readOnly: this.readonly,
+            formats: this.formats,
+            bounds: this.bounds,
+            debug: this.debug,
+            scrollingContainer: this.scrollingContainer
+        });
+
+        if (this.value) {
+            this.quill.pasteHTML(this.value);
+        }
+
+        this.quill.on('text-change', (delta, oldContents, source) => {
+            this.textContent = oldContents;
+            if (source === 'user') {
+                let html = editorElement.children[0].innerHTML;
+                const text = this.quill.getText().trim();
+                if (html === '<p><br></p>') {
+                    html = null;
+                }
+
+                this.onTextChange.emit({
+                    htmlValue: html,
+                    textValue: text,
+                    delta,
+                    source
+                });
+            }
+        });
+
+        this.quill.on('selection-change', (range, oldRange, source) => {
+            this.cursorPosition = oldRange;
+            this.onSelectionChange.emit({
+                range,
+                oldRange,
+                source
+            });
+        });
+
+        this.onInit.emit({
+            editor: this.quill
+        });
+    }
 
     initForm(item) {
         this.itemForm = this.fb.group({
@@ -125,6 +225,12 @@ export class EmailTemplateItemComponent implements OnInit {
     }
 
     onChangePlaceholder(event) {
-        console.log(event);
+        const inset = this.textContent ? this.textContent.ops[0].insert : '';
+        const index = this.cursorPosition ? this.cursorPosition.index : 0;
+        const editor_content = [inset.slice(0, index), event.value, inset.slice(index)].join('');
+        this.quill.setContents({
+            ops: [{ insert: editor_content }]
+        });
+        // this.itemForm.controls['content'].setValue(a);
     }
 }
