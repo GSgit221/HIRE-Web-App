758c41d53b6c8790cb18128652a9e0f526a9d631
nickrockstar <wezawe@gmail.com>
2019-07-15 12:53:32 +0200

    Stage title

--- src/app/modules/shared/stage-column-height.directive.ts
+++ src/app/modules/shared/stage-column-height.directive.ts
@@ -6,7 +6,9 @@ import { AfterViewInit, Directive, ElementRef, Inject, OnDestroy } from '@angula
 })
 export class StageColumnHeightDirective implements AfterViewInit, OnDestroy {
     w: any = window;
-    constructor(private el: ElementRef, @Inject(DOCUMENT) document) {}
+    constructor(private el: ElementRef, @Inject(DOCUMENT) document) {
+        this.setHeight = this.setHeight.bind(this);
+    }
 
     setHeight() {
         const windowHeight = this.w.innerHeight;
@@ -14,7 +16,7 @@ export class StageColumnHeightDirective implements AfterViewInit, OnDestroy {
         const headerHeight = document.querySelector('.container .header-block').clientHeight;
         const separatorHeight = 21;
         const stageHeaderHeight = document.querySelector('.container .stage-header').clientHeight + 12;
-        const heightAvailable = windowHeight - padding - headerHeight - separatorHeight - stageHeaderHeight - 30;
+        const heightAvailable = windowHeight - padding - headerHeight - separatorHeight - stageHeaderHeight + 10;
         if (this.el.nativeElement) {
             this.el.nativeElement.style.maxHeight = heightAvailable + 'px';
         }
@@ -22,14 +24,10 @@ export class StageColumnHeightDirective implements AfterViewInit, OnDestroy {
 
     ngAfterViewInit() {
         this.setHeight();
-        this.w.addEventListener('resize', () => {
-            this.setHeight();
-        });
+        this.w.addEventListener('resize', this.setHeight, false);
     }
 
     ngOnDestroy(): void {
-        this.w.removeEventListener('resize', () => {
-            this.setHeight();
-        });
+        this.w.removeEventListener('resize', this.setHeight, false);
     }
 }
