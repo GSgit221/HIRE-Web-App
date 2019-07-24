58e778312f6cd0a757b1dbb6cf92aae64cf9a20d
nickrockstar <wezawe@gmail.com>
2019-07-24 19:27:16 +0200

    SSO

--- src/app/modules/auth/sso/sso.component.ts
+++ src/app/modules/auth/sso/sso.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormHelperService } from '@app/core/services/form-helper.service';
 import { Message } from 'primeng/components/common/api';
+import { filter } from 'rxjs/operators';
 import { AuthService } from '../auth.service';
+import { UtilitiesService } from './../../../core/services/utilities.service';
 
 @Component({
     selector: 'app-sso',
@@ -19,13 +21,33 @@ export class SSOComponent implements OnInit {
         private fb: FormBuilder,
         private formHelper: FormHelperService,
         private authService: AuthService,
-        private router: Router
+        private router: Router,
+        private route: ActivatedRoute,
+        private utilities: UtilitiesService
     ) {
         this.ssoForm = this.fb.group({
             company: ['', Validators.required]
         });
     }
-    ngOnInit() {}
+    ngOnInit() {
+        this.route.queryParams.subscribe((queryParams) => {
+            if (queryParams.error && queryParams.error.length) {
+                this.msgs = [];
+                this.msgs.push({ severity: 'error', detail: queryParams.error || 'Error' });
+            }
+            console.log(queryParams);
+            if (queryParams.tenant_id && queryParams.access_token && queryParams.user_hash) {
+                const response = {
+                    tenant_id: queryParams.tenant_id,
+                    access_token: queryParams.access_token,
+                    user_hash: queryParams.user_hash
+                };
+                this.authService.setSession(response);
+                this.utilities.setTenant(response.tenant_id);
+                this.router.navigateByUrl(`tenant/${response.tenant_id}/hire`);
+            }
+        });
+    }
 
     onssoSignIn(event) {
         event.preventDefault();
