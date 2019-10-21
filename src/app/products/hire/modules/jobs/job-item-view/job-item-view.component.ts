dce461826df6097b41a5efecf0c524338652a4bd
alonavns <alona.vns@outlook.com>
2019-10-21 08:13:43 +0300

    fix issue

--- src/app/products/hire/modules/jobs/job-item-view/job-item-view.component.ts
+++ src/app/products/hire/modules/jobs/job-item-view/job-item-view.component.ts
@@ -370,6 +370,9 @@ export class JobItemViewComponent implements OnInit, OnDestroy, AfterViewInit {
 
     getQuestionsClass(candidate) {
         const _candidateQuestions = this.candidateQuestions[candidate.id];
+
+        if (!_candidateQuestions) return 'grey';
+
         if ((candidate.hasUser && candidate.hasUserReviewed) || candidate.matching) {
             if (_candidateQuestions.hasAnswers) {
                 return _candidateQuestions.isKnockout ? 'red' : 'green';
